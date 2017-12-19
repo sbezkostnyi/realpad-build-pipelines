bfc05bd8fa4b5281e46c8953c03d85b2e3049afc
Sergey Bezkostnyi <sergey@easycoremedia.com>
2017-12-19 15:31:27 +0200

    Added script to send static code analyzers reports

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,11 @@ FROM frekele/gradle:4-jdk8
 
 # Install required packages
 RUN apt-get update \
-    && apt-get install -y git file locales \
+    && apt-get install -y git file locales heirloom-mailx \
     && rm -rf /var/lib/apt/lists/*
+
+# Add script to send static code analyzers reports
+ADD send_reports.sh /send_reports.sh
 
 # Make the "en_US.UTF-8" locale
 RUN localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
---
+++ send_reports.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Script sends email with static code analyzers reports from Gradle build
+# It designed to be executed as part of Bitbucket Pipelines build
+
+# Requires to configure next environment variables in your repository:
+# SMTP_SERVER=host:port
+# SMTP_AUTH_USER=username
+# SMTP_AUTH_PASSWORD=password
+# SMTP_SENDER=from@domain.com
+# SMTP_RECIPIENTS=recipient@domain.com
+
+TEMP_DIR="/tmp/reports"
+
+# Cleanup temporary directory
+if [[ -d "${TEMP_DIR}" ]]; then
+    rm -rf "${TEMP_DIR}"
+fi
+mkdir "${TEMP_DIR}"
+
+REPORTS_DIR="${BITBUCKET_CLONE_DIR}/build/reports/"
+ATTACHMENTS=()
+
+# Copy reports to temporary directory
+for DIR in ${REPORTS_DIR}*/; do
+  DIR_NAME=$(basename "${DIR}")
+  for FILE in ${DIR}*; do
+    FILE_NAME=$(basename "${FILE}")
+    REPORT_FILE="${TEMP_DIR}/report.${DIR_NAME}.${FILE_NAME}"
+    cp "${FILE}" "${REPORT_FILE}"
+    ATTACHMENTS+=("-a ${REPORT_FILE}")
+  done
+done
+
+# Build email body
+echo "Static code analyzers reports from the Pipelines build #${BITBUCKET_BUILD_NUMBER}" > "${TEMP_DIR}/body.txt"
+echo "" >> "${TEMP_DIR}/body.txt"
+echo "Build details: https://bitbucket.org/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/addon/pipelines/home#!/results/${BITBUCKET_BUILD_NUMBER} " >> "${TEMP_DIR}/body.txt"
+echo "Commit details: https://bitbucket.org/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/commits/${BITBUCKET_COMMIT} " >> "${TEMP_DIR}/body.txt"
+
+# Send email with reports
+heirloom-mailx -v -r "${SMTP_SENDER}" -s "Static code analyzers reports from the Pipelines build #${BITBUCKET_BUILD_NUMBER}" ${ATTACHMENTS[@]} -S smtp="${SMTP_SERVER}" -S smtp-use-starttls -S smtp-auth="login" -S smtp-auth-user="${SMTP_AUTH_USER}" -S smtp-auth-password="${SMTP_AUTH_PASSWORD}" ${SMTP_RECIPIENTS} < ${TEMP_DIR}/body.txt
